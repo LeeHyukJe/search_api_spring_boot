0b23869060ddf228afac19061e49d55ac882d482
leehyukje <lims2733@naver.com>
2020-11-05 14:41:10 +0900

    검색 API 테스트 생성

---
+++ src/test/java/com/wisenut/search/domain/model/SearchManagementTests.java
@@ -0,0 +1,10 @@
+package com.wisenut.search.domain.model;
+
+import com.wisenut.search.web.payload.SearchPayload;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SearchManagementTests {
+
+
+}
